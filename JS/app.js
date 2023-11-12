638bea5ac3cc1aac8dbc2831569cb208e893547d
ninalek <mylekyanina@gmail.com>
2023-11-12 20:24:10 -0300

    agregando checkbox a la lista

--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,7 @@ const tarea= document.getElementById('tarea');
 const btnAgregar = document.getElementById('btnAgregar');
 const lista = document.getElementById('lista');
 btnAgregar.addEventListener('click',()=>{
-    lista.innerHTML=tarea.value
+    lista.innerHTML +=` <input type="checkbox" class="tarea-op"> ${tarea.value} <br>`
     tarea.value='';
 });
+
