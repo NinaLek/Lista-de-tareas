4d42169365ae91b7f46626d9ec79c55f095774be
ninalek <mylekyanina@gmail.com>
2023-11-12 21:03:53 -0300

    Agregando func eliminar, acomodando agregar

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,42 @@
-const tarea= document.getElementById('tarea');
-const btnAgregar = document.getElementById('btnAgregar');
 const lista = document.getElementById('lista');
-btnAgregar.addEventListener('click',()=>{
-    lista.innerHTML +=` <input type="checkbox" class="tarea-op"> ${tarea.value} <br>`
-    tarea.value='';
+const tarea= document.getElementById('tarea');
+//----------------botones----------------------------------------
+const btnAgregar = document.getElementById('btnAgregar');
+const btnEliminar = document.getElementById('btnEliminar');
+
+//---------------------------------------------------------------
+
+//------funciones--------------------------------------------------
+
+function eliminarSeleccion(){
+    const checkbox = document.getElementsByClassName('tarea-op');
+    for(const elem of checkbox){
+        if(elem.checked){
+            console.log('a ver');
+            elem.nextSibling.remove();
+            elem.remove();
+        }
+    }
+}
+
+function agregarTarea(){
+    if(tarea.value != ''){
+        lista.innerHTML +=`<li><input type="checkbox" class="tarea-op"> ${tarea.value}</li>`
+        tarea.value='';
+    }else{
+        document.getElementById('advertencia').innerHTML = 'Ingrese la tarea que quiere agregar';
+    }
+}
+
+
+//------Event Listeners--------------------------------------------
+
+btnAgregar.addEventListener('click',()=> {
+    agregarTarea();
 });
 
+
+
+btnEliminar.addEventListener('click',()=> {
+    eliminarSeleccion();
+});
